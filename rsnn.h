0720cf9124520eeab66d6547a2dd29a4eac22805
Luciano Prono <luciano.prono@polito.it>
2023-01-23 16:36:18 +0100

    Create rsnn.h

---
+++ rsnn.h
@@ -0,0 +1,33 @@
+// RSNN CONFIG
+#define SCALE_FACTOR                0
+#define OUTPUT_SCALE_FACTOR         4
+#define FIRING_THRESHOLD            77
+#define ALPHA_DECAY                 32637
+#define KAPPA_DECAY                 31232
+
+#define INPUT_NEURONS_NUM 578
+#define RECURRENT_NEURONS_NUM 100
+#define OUTPUT_NEURONS_NUM 10
+
+#define WIN_BUFF_SIZE 57800
+#define WREC_BUFF_SIZE 10000
+#define WOUT_BUFF_SIZE 1000
+
+uint16_t input_size = INPUT_NEURONS_NUM;
+
+int8_t layer0_w[WIN_BUFF_SIZE];
+int8_t layer0_rw = wrec_buff[WREC_BUFF_SIZE];
+int8_t layer1_w = wout_buff[WOUT_BUFF_SIZE];
+
+int32_t layer0_v[RECURRENT_NEURONS_NUM];
+uint16_t layer0_size = RECURRENT_NEURONS_NUM;
+uint16_t layer0_z[RECURRENT_NEURONS_NUM];
+uint16_t layer0_z_num;
+int32_t layer1_v[OUTPUT_NEURONS_NUM];
+uint16_t layer1_size = OUTPUT_NEURONS_NUM;
+
+void rsnn_reset();
+void rsnn_update(uint16_t* input_z, int32_t* output_potentials, uint16_t input_size, uint16_t output_size, uint16_t input_z_num);
+void apply_leakage(int32_t* potentials, uint16_t n, uint32_t leakage);
+void apply_spikes(int32_t* potentials, uint16_t* spikes, int8_t* weights, uint16_t n, uint16_t m, uint16_t spikes_num, uint8_t w_scale);
+void generate_spikes(int32_t* potentials, uint16_t* spikes, uint16_t n, int32_t threshold, uint16_t* spikes_num_p);
